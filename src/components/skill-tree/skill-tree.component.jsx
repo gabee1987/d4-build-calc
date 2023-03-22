db21839a3902f4af1ca75f4150ad2211ec431c23
gabee1987 <gabee1987@gmail.com>
2023-03-22 19:49:48 +0100

    uninstalled the beautiful-skill-tree library due to the lack of customizable moduls

--- src/components/skill-tree/skill-tree.component.jsx
+++ src/components/skill-tree/skill-tree.component.jsx
@@ -13,37 +13,7 @@ import {
 
 const SkillTreeComponent = () => {
   const [nodes, setNodes] = useState([]);
-  const data = [
-    {
-      id: "hello-world",
-      title: "Hello World",
-      tooltip: {
-        content:
-          "This node is the top most level, and will be unlocked, and ready to be clicked.",
-      },
-      children: [
-        {
-          id: "hello-sun",
-          title: "Hello Sun",
-          tooltip: {
-            content:
-              "This is a parent of the top node, and will locked while the parent isn’t in a selected state.",
-          },
-          children: [],
-        },
-        {
-          id: "hello-stars",
-          title: "Hello Stars",
-          tooltip: {
-            content:
-              "This is the child of ‘Hello World and the sibling of ‘Hello Sun’. Notice how the app takes care of the layout automatically? That’s why this is called Beautiful Skill Tree and not just ‘Skill Tree’. (Also the npm namespace had already been taken for the latter so (flick hair emoji).",
-          },
-          children: [],
-        },
-      ],
-    },
-  ];
-
+  
   useEffect(() => {
     setNodes(sorcererData.nodes);
     console.log(nodes);
@@ -59,38 +29,6 @@ const SkillTreeComponent = () => {
   };
 
   return (
-    <SkillProvider>
-      <SkillTreeGroup>
-        {() => {
-          return (
-            <SkillTree
-              treeId="treeOne"
-              title="Save Example"
-              data={data} // defined elsewhere
-              //   handleSave={handleSave}
-              //   savedData={savedData}
-            />
-          );
-        }}
-      </SkillTreeGroup>
-    </SkillProvider>
-
-    // <SkillProvider>
-    //   <SkillTreeGroup>
-    //     <SkillTree>
-    //       {nodes.map((node) => (
-    //         <SkillTree.Node
-    //           key={node.id}
-    //           id={node.id}
-    //           title={node.name}
-    //           description={node.description}
-    //           learned={node.learned}
-    //           onClick={() => onNodeClick(node.id)}
-    //         />
-    //       ))}
-    //     </SkillTree>
-    //   </SkillTreeGroup>
-    // </SkillProvider>
 
     // <div>
     //   <SkillTree
