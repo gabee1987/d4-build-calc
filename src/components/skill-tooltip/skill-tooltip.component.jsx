578b7223715c7b65f7f3c8aa98acbba062bb2802
gabee1987 <gabee1987@gmail.com>
2023-04-05 21:00:08 +0200

    created the tooltip component design, added frame and background design, finetuned the title text of the tooltip

--- src/components/skill-tooltip/skill-tooltip.component.jsx
+++ src/components/skill-tooltip/skill-tooltip.component.jsx
@@ -19,43 +19,45 @@ const SkillTooltipComponent = ({
         top: position.y,
       }}
     >
-      <div className="title-container">
-        <h1>{nodeData.name}</h1>
-      </div>
-      <div className="separator"></div>
-      <p>Type: {nodeData.nodeType}</p>
-      <div className="allocated-max-points">
-        <span>{nodeData.allocatedPoints}</span> <span>/</span>{" "}
-        <span>{nodeData.maxPoints}</span>
-      </div>
-      {nodeData.description && (
-        <div className="description">
-          {nodeData.description &&
-            populateDescriptionNumbers(
-              nodeData.description,
-              descriptionValues,
-              descriptionExtraValues
-            )
-              .split("\n")
-              .map((line, index) => {
-                if (line.startsWith("Lucky Hit Chance")) {
-                  return (
-                    <p key={index} className="lucky-hit-chance">
-                      {line}
-                    </p>
-                  );
-                } else if (line.includes("— Enchantment Effect —")) {
-                  return (
-                    <p key={index} className="enchantment-effect">
-                      {line}
-                    </p>
-                  );
-                } else {
-                  return <p key={index}>{line}</p>;
-                }
-              })}
+      <div className="tooltip-container">
+        <div className="title-container">
+          <h1>{nodeData.name}</h1>
         </div>
-      )}
+        <div className="separator"></div>
+        <p>Type: {nodeData.nodeType}</p>
+        <div className="allocated-max-points">
+          <span>{nodeData.allocatedPoints}</span> <span>/</span>{" "}
+          <span>{nodeData.maxPoints}</span>
+        </div>
+        {nodeData.description && (
+          <div className="description">
+            {nodeData.description &&
+              populateDescriptionNumbers(
+                nodeData.description,
+                descriptionValues,
+                descriptionExtraValues
+              )
+                .split("\n")
+                .map((line, index) => {
+                  if (line.startsWith("Lucky Hit Chance")) {
+                    return (
+                      <p key={index} className="lucky-hit-chance">
+                        {line}
+                      </p>
+                    );
+                  } else if (line.includes("— Enchantment Effect —")) {
+                    return (
+                      <p key={index} className="enchantment-effect">
+                        {line}
+                      </p>
+                    );
+                  } else {
+                    return <p key={index}>{line}</p>;
+                  }
+                })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
@@ -73,8 +75,6 @@ function populateDescriptionNumbers(
     result = result.replace("{#}", value);
   });
 
-  console.log(result);
-  console.log(descriptionExtraValues);
   // Replacing the extra values (Enchantment Effect) if there are extra values
   if (descriptionExtraValues && result) {
     descriptionExtraValues.forEach((extraValue) => {
