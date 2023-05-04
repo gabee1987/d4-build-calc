881cd547bc237962e40b366017f9946462786a92
gabee1987 <gabee1987@gmail.com>
2023-05-04 15:38:02 +0200

    changed back react-scripts from react-rewired

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
-module.exports = function override(config, env) {
-  config.plugins.push(new BundleAnalyzerPlugin());
-  return config;
-};
