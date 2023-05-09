889ffc91fca75010cbff641ee5a81ed4e10ad344
gabee1987 <gabee1987@gmail.com>
2023-05-09 15:04:43 +0200

    finished the build profiles panel design

--- src/components/build-profiles/build-profiles.component.jsx
+++ src/components/build-profiles/build-profiles.component.jsx
@@ -85,9 +85,7 @@ const BuildProfiles = ({ onClose }) => {
           <button
             className="build-profiles-close-button"
             onClick={toggleIsOpen}
-          >
-            X
-          </button>
+          ></button>
           <div className="build-profiles-title-container">
             <h2>Save/Load Builds</h2>
           </div>
@@ -141,7 +139,16 @@ const BuildProfiles = ({ onClose }) => {
                         className="delete-build-button"
                         onClick={() => handleDelete(profile)}
                       >
-                        Delete
+                        <svg
+                          viewBox="0 -8 40 40"
+                          aria-hidden="true"
+                          focusable="false"
+                          fill="currentColor"
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="trashcan-icon"
+                        >
+                          <path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm4 12H8v-9h2v9zm6 0h-2v-9h2v9zm.618-15L15 2H9L7.382 4H3v2h18V4z"></path>
+                        </svg>
                       </button>
                     </div>
                   </li>
