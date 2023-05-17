d76a4a7d4a3c768d98202300fc78047c60bc6459
gabee1987 <gabee1987@gmail.com>
2023-05-17 20:24:24 +0200

    started to create a class overview page, only the skeleton is complete yet and the class menu selection left-right click modification

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ClassSelectionContext from "./contexts/class-selection.context";
 
 // Components
 import ClassMenu from "./components/class-menu/class-menu.component.jsx";
+import ClassOverviewComponent from "./components/class-overview/class-overview.component.jsx";
 import SkillCalculator from "./components/skill-calculator/skill-calculator.component.jsx";
 import IndexPage from "./components/index-page/index-page.component";
 import Footer from "./components/footer/footer.component";
@@ -29,6 +30,10 @@ const App = () => {
       <Routes>
         <Route index path="/" element={<IndexPage />} />
         <Route path="/class-menu" element={<ClassMenu />} />
+        <Route
+          path="/class-info/:className"
+          element={<ClassOverviewComponent />}
+        />
         <Route element={<Footer />}></Route>
         <Route
           path="/skill-tree/:className/:allocatedPoints?"
