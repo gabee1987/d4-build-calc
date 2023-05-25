f4362c8d7846480bd0e61359c931c855a7d67ebd
gabee1987 <gabee1987@gmail.com>
2023-05-25 15:55:14 +0200

    finished the class-skills panel design

--- src/components/class-skills/class-skills.component.jsx
+++ src/components/class-skills/class-skills.component.jsx
@@ -3,12 +3,26 @@ import React, { useContext, useEffect, useState } from "react";
 import Skill from "../skill/skill.component.jsx";
 import { loadSkills } from "../../helpers/codex/class-skills-loader.js";
 
+import classIconBarbarian from "../../assets/icons/class-icon-barbarian.webp";
+import classIconNecromancer from "../../assets/icons/class-icon-necromancer.webp";
+import classIconSorcerer from "../../assets/icons/class-icon-sorcerer.webp";
+import classIconRogue from "../../assets/icons/class-icon-rogue.webp";
+import classIconDruid from "../../assets/icons/class-icon-druid.webp";
+
 import "./class-skills.styles.scss";
 
 const ClassSkillsComponent = () => {
   const [skills, setSkills] = useState([]);
   const [selectedClasses, setSelectedClasses] = useState([]);
   const classes = ["Barbarian", "Necromancer", "Sorcerer", "Rogue", "Druid"];
+
+  const classIcons = {
+    Barbarian: classIconBarbarian,
+    Necromancer: classIconNecromancer,
+    Sorcerer: classIconSorcerer,
+    Rogue: classIconRogue,
+    Druid: classIconDruid,
+  };
 
   useEffect(() => {
     let activeSkills = loadSkills();
@@ -38,7 +52,7 @@ const ClassSkillsComponent = () => {
       <div className="class-skills-navbar">
         <div className="class-skill-nav-left">
           <div className="class-skills-title-container">
-            <h1>Diablo 4 Class Skills</h1>
+            <h2>Diablo 4 Class Skills</h2>
           </div>
         </div>
         <div className="class-skill-nav-right"></div>
@@ -46,6 +60,7 @@ const ClassSkillsComponent = () => {
       <div className="class-skills-container">
         <div className="class-skills-content">
           <div className="class-skills-header-container"></div>
+          <div className="class-skills-header-container-center"></div>
           <div className="class-skills-content-bg-container"></div>
           <div className="class-skills-class-selection inner-panel">
             <ul className="class-skills-class-list">
@@ -57,6 +72,7 @@ const ClassSkillsComponent = () => {
                     selectedClasses.includes(className) ? "class-selected" : ""
                   }
                 >
+                  <img src={classIcons[className]} alt={className} />
                   {className}
                 </li>
               ))}
