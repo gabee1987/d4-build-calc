e1e18aba25cd97135e1fe40e562094d07999bb39
gabee1987 <gabee1987@gmail.com>
2023-05-31 15:53:19 +0200

    added the aspect icons for the aspect items in the list, finetuned the text design

--- src/components/aspect/aspect.component.jsx
+++ src/components/aspect/aspect.component.jsx
@@ -1,43 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { loadAspectIcons } from "../../helpers/codex/aspects-loader";
 
 import "./aspect.styles.scss";
 
 const Aspect = ({ data }) => {
   const { name, class: aspectClass, slots, description, category } = data;
-  //const skillNameNormalized = normalizeSpellName(data.name);
-  //const aspectImage = aspectImagesMap[skillNameNormalized];
+  const [aspectIcon, setAspectIcon] = useState(null);
 
   const renderSkillIcon = () => (
-    <div className="skill-icon-container">
-      <img src={0} className="skill-icon" alt={data.name} />
+    <div className="aspect-icon-container">
+      <img className="aspect-icon" src={aspectIcon} alt={name} />
     </div>
   );
+
+  useEffect(() => {
+    loadAspectIcons(category)
+      .then((icon) => {
+        setAspectIcon(icon);
+      })
+      .catch((error) => {
+        console.error("Failed to load aspect icon:", error);
+      });
+  }, [category]);
 
   return (
     <li className="aspect">
       <div className="aspect-bg-container"></div>
       <div className="aspect-horizontal-center"></div>
       <div className="aspect-vertical-center"></div>
+      {renderSkillIcon()}
       <div className="aspect-details">
         <div className="aspect-header">
           <div className="aspect-name">
             <h2>{name}</h2>
             <div className="aspect-class">
-              <span>{category}</span> • <b>{aspectClass}</b>
+              <span>{category}</span> • {aspectClass}
             </div>
           </div>
         </div>
         <div className="aspect-resources">
           <div className="aspect-resource">
-            <span className="aspect-resource-label">Slots:</span>{" "}
+            <span className="aspect-resource-label">
+              <b>Slots:</b>
+            </span>{" "}
             {slots.join(", ")}
-          </div>
-          <div className="aspect-resource">
-            <img
-              className="aspect-resource-icon"
-              //   src={`https://rerollcdn.com/DIABLO4/Codex/1/${category}.png`}
-              alt={name}
-            />
           </div>
         </div>
         <div className="aspect-description">{description}</div>
--- src/helpers/codex/aspects-loader.js
+++ src/helpers/codex/aspects-loader.js
@@ -3,3 +3,36 @@ import aspectsData from "../../data/aspects/aspects-data.json";
 export const loadAspects = () => {
   return aspectsData;
 };
+
+export const loadAspectIcons = (category) => {
+  let iconFileName;
+  switch (category) {
+    case "defensive":
+      iconFileName = "aspect-defensive.webp";
+      break;
+    case "mobility":
+      iconFileName = "aspect-mobility.webp";
+      break;
+    case "offensive":
+      iconFileName = "aspect-offensive.webp";
+      break;
+    case "resource":
+      iconFileName = "aspect-resource.webp";
+      break;
+    case "utility":
+      iconFileName = "aspect-utility.webp";
+      break;
+    default:
+      // TODO Handle default case
+      break;
+  }
+
+  // Load the aspect icon file dynamically
+  if (iconFileName) {
+    return import(`../../assets/icons/${iconFileName}`).then(
+      (icon) => icon.default
+    );
+  }
+
+  return Promise.reject(new Error(`Invalid aspect category: ${category}`));
+};
